d8251e19f719f7847d82554b3dd195d85980fd99
f1schmann <f1schmann@eclipso.de>
2023-10-01 13:58:39 +0200

    small cleanup and added OctetBlock-Class

---
+++ src/main/java/net/f1schmann/octetmod/OctetBlock.java
@@ -0,0 +1,11 @@
+package net.f1schmann.octetmod;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+
+public class OctetBlock extends Block implements SimpleWaterloggedBlock {
+
+    public OctetBlock(Properties pProperties) {
+        super(pProperties);
+    }
+}
